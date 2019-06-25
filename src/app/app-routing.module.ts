c3988c120da129955baa794e498b3b96470c2942
Jérome Cristante <jerome.cristante@its4u.lu>
2019-06-25 13:19:52 +0200

    Secure application with SSO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';import { ProductListComponent } from './features/product-list/product-list.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './features/product-list/product-list.component';
+import { AppAuthGuard } from './app-auth-guard';
 
 const routes: Routes = [
-      { path: '', component: ProductListComponent },
+      { 
+        path: '',
+        component: ProductListComponent,
+        canActivate: [AppAuthGuard],
+        data: {
+          roles: ['Listing_Product']
+        }
+     },
 ];
 
 @NgModule({
